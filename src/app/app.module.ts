1a5bddddf6e987f202660239a57d89c68578fe83
Damian Łączyński <damian.laczynski.edu@gmail.com>
2024-03-23 14:58:55 +0100

    Update user-form, user-profile and create-user-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,11 @@ import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { HomeComponent } from './home/view/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule, HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
-import { UserProfileComponent } from './user/components/user-profile/view/user-profile.component';
 import { OrganizationHomePageComponent } from './organization/components/organization-home-page/view/organization-home-page.component';
 import { OrganizationSettingsComponent } from './organization/components/organization-settings/view/organization-settings.component';
 import { UserOrganizationComponent } from './user/components/user-organization/view/user-organization.component';
 import { OrganizationCreationComponent } from './organization/view/organization-creation/view/organization-creation.component';
 import { NoOrganizationComponent } from './organization/view/no-organization/view/no-organization.component';
-import { UserInvitationsComponent } from './invitations/view/user-invitations/view/user-invitations.component';
 import { ListMembersComponent } from './organization/components/organization-members/view/list-members/list-members.component';
 import { OrganizatonNavComponent } from './organization/components/organizaton-nav/organizaton-nav.component';
 import { OrganizationComponent } from './organization/organization/organization.component';
@@ -35,13 +33,11 @@ import { TokenInterceptor } from './auth/interceptor/token.interceptor';
     HomeComponent,
     NavMenuComponent,
     FooterComponent,
-    UserProfileComponent,
     OrganizationHomePageComponent,
     OrganizationSettingsComponent,
     UserOrganizationComponent,
     OrganizationCreationComponent,
     NoOrganizationComponent,
-UserInvitationsComponent,
     ListMembersComponent,
     OrganizatonNavComponent,
     OrganizationComponent,
--- src/app/user/components/update-user-profile/create-user-profile.component.ts
+++ src/app/user/components/update-user-profile/create-user-profile.component.ts
@@ -2,27 +2,27 @@ import { Component, inject } from '@angular/core';
 import { UserFormComponent } from '../user-form/user-form.component';
 import { CreateUserRequest } from '../../model/create-user-request';
 import { UserService } from '../../service/user.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-create-user-profile',
   standalone: true,
-  imports: [UserFormComponent],
+  imports: [UserFormComponent, NgIf],
   templateUrl: './create-user-profile.component.html',
-  styleUrls: ['./create-user-profile.component.css']
+  styleUrls: ['./create-user-profile.component.css'],
 })
 export class UpdateUserProfileComponent {
-
   private _userService = inject(UserService);
 
-  createUser(user: CreateUserRequest)
-  {
-    this._userService.create(user).subscribe(
-      (response) => {
+  createUser(user: CreateUserRequest) {
+    console.log(user);
+    this._userService.create(user).subscribe({
+      next: (response) => {
         console.log('User created successfully:', response);
       },
-      (error) => {
+      error: (error) => {
         console.error('User creation failed:', error);
-      }
-    );
+      },
+    });
   }
 }
