3fe1d007bfb68eeb58b4ac350996946f4906e2cd
Damian Łączyński <damian.laczynski.edu@gmail.com>
2024-03-27 13:12:49 +0100

    Update app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,48 +5,64 @@ import { UserSettingsComponent } from './user/components/user-settings/view/user
 import { HomeComponent } from './home/view/home.component';
 import { LoginComponent } from './login/view/login.component';
 import { RegisterComponent } from './register/view/register.component';
-import { NoOrganizationComponent } from './organization/view/no-organization/view/no-organization.component';
 import { AuthGuard } from './jwt/auth.guard';
 import { OrganizationCreationComponent } from './organization/view/organization-creation/view/organization-creation.component';
 import { OrganizationComponent } from './organization/organization/organization.component';
 import { OrganizationHomePageComponent } from './organization/components/organization-home-page/view/organization-home-page.component';
-import { ChooseOrganizationComponent } from './organization/view/choose-organization/view/choose-organization.component';
 import { ListMembersComponent } from './organization/components/organization-members/view/list-members/list-members.component';
 import { AddMembersComponent } from './organization/components/organization-members/view/add-members/add-members.component';
 import { OrganizationScheduleComponent } from './organization/components/organization-schedule/organization-schedule.component';
 import { OrganizationSettingsComponent } from './organization/components/organization-settings/view/organization-settings.component';
 import { OrganizationTasksComponent } from './organization/components/organization-tasks/organization-tasks.component';
 import { OrganizationDocumentsComponent } from './organization/components/organization-documents/organization-documents.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.page.component';
+import { OrganizationListComponent } from './organization/view/organization-list/organization-list.page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
-  { path: 'welcome', component: HomeComponent},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard/:userId',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'welcome', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'user/:id', component: UserProfileComponent, canActivate: [AuthGuard] },
-  { path: 'user/:userId/settings', component: UserSettingsComponent, canActivate: [AuthGuard] },
-  { path: 'create-organization', component: OrganizationCreationComponent, canActivate: [AuthGuard] },
-  { path: 'noorganization', component: NoOrganizationComponent, canActivate: [AuthGuard] },
-  { path: 'chooseorganization', component: ChooseOrganizationComponent, canActivate: [AuthGuard] },
   {
-    path: 'organization/:id',
+    path: 'user/:id',
+    component: UserProfileComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'user/:userId/settings',
+    component: UserSettingsComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'organization/create',
+    component: OrganizationCreationComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'organization', component: OrganizationListComponent },
+  {
+    path: 'organization/:organizationId',
     component: OrganizationComponent,
     children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: OrganizationHomePageComponent },
       { path: 'schedule', component: OrganizationScheduleComponent },
       { path: 'members', component: ListMembersComponent },
       { path: 'settings', component: OrganizationSettingsComponent },
       { path: 'tasks', component: OrganizationTasksComponent },
       { path: 'documents', component: OrganizationDocumentsComponent },
-      { path: 'addMembers',component: AddMembersComponent}
-    ]
+      { path: 'addMembers', component: AddMembersComponent },
+    ],
   },
-  { path: '**', redirectTo: '/welcome' },
-
+  { path: '**', redirectTo: '/dashboard' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { bindToComponentInputs: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
