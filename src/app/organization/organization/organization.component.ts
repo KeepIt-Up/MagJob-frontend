156a98f3af5f3b4effdc0db3354fa48648c394fc
Damian Łączyński <damian.laczynski.edu@gmail.com>
2024-03-27 13:19:39 +0100

    Update routerLink paths in organization-nav

--- src/app/organization/organization/organization.component.ts
+++ src/app/organization/organization/organization.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { OrganizatonNavComponent } from '../components/organizaton-nav/organizaton-nav.component';
 import { RouterOutlet } from '@angular/router';
+import { ListMembersComponent } from '../components/organization-members/view/list-members/list-members.component';
 
 @Component({
   selector: 'app-organization',
@@ -10,5 +11,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./organization.component.css']
 })
 export class OrganizationComponent {
+  @Input() organizationId?: string;
 
+  handleRouterActivation(component: any) {
+    if(component instanceof ListMembersComponent)
+    {
+      component.organizationId = this.organizationId;
+    }
+  }
 }
