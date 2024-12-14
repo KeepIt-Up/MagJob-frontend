c2f3f3224e0d2c81d2afde4804d533859500ab88
WallyS02 <sebkutny@gmail.com>
2024-12-14 15:04:18 +0100

    Fixed issuer hostname conflicts

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -1,11 +1,13 @@
-import { AuthConfig, OAuthService } from "angular-oauth2-oidc";
+import { AuthConfig } from "angular-oauth2-oidc";
 
 export const authCodeFlowConfig: AuthConfig = {
-    issuer: 'http://localhost:8180/realms/master',
-    tokenEndpoint: 'http://localhost:8180/realms/master/protocol/openid-connect/token',
+    issuer: 'http://localhost:8080/realms/master',
+    tokenEndpoint: 'http://localhost:8080/realms/master/protocol/openid-connect/token',
     redirectUri: window.location.origin + "/welcome",
     clientId: 'magjob-angular-client',
     responseType: 'code',
     scope: 'openid profile',
+    requireHttps: false,
+    strictDiscoveryDocumentValidation: false,
+    showDebugInformation: true,
 }
-
